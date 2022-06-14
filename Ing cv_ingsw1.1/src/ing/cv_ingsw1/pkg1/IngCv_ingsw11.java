6249a24bb97d368b317146d7a907153d7fe0d9e0
Usuario <Usuario@Usuario-PC>
2022-06-14 17:19:15 -0500

    version 1

---
+++ Ing cv_ingsw1.1/src/ing/cv_ingsw1/pkg1/IngCv_ingsw11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing.cv_ingsw1.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class IngCv_ingsw11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("versión 1");
+    }
+    
+}
